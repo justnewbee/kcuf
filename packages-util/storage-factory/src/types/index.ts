f018e5a733f53c25d3909900f10e2f8972ad0e5b
Jianchun Wang <justnewbee@gmail.com>
2024-10-16 10:01:05 +0800

    fix(storage-factory): type naming was misleading

--- packages-util/storage-factory/src/types/index.ts
+++ packages-util/storage-factory/src/types/index.ts
@@ -1,7 +1,16 @@
 export interface IStorageFn<T extends object> {
+  /**
+   * 获取整个对象
+   */
   (): T;
+  /**
+   * 部分更新（单项）
+   */
   update<K extends keyof T>(key: K, value: T[K]): T;
-  update(wholeData: Partial<T>): T;
+  /**
+   * 部分更新（多项）
+   */
+  update(partial: Partial<T>): T;
   /**
    * 全量更新
    */
