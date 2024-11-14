a9f2049a36ef7afda47bdc0b4e030d48b3e78283
Jianchun Wang <justnewbee@gmail.com>
2024-11-14 19:15:31 +0800

    chore: up version ✨

--- packages-config/eslint-config/config-import.js
+++ packages-config/eslint-config/config-import.js
@@ -4,6 +4,20 @@ import pluginImport from 'eslint-plugin-import';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  {
+    settings: {
+      // make aliases work, need `eslint-import-resolver-typescript`
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+          project: [
+            'tsconfig.json',
+            'packages*/*/tsconfig.json'
+          ]
+        }
+      }
+    }
+  },
   pluginImport.flatConfigs.recommended,
   pluginImport.flatConfigs.typescript,
   pluginImport.flatConfigs.react,
