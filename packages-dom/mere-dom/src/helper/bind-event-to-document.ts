f9cf98e67fa9df39cc4bc7c0344ede5cbb011d1f
Jianchun Wang <justnewbee@gmail.com>
2025-06-10 10:34:06 +0800

    feat: add bindEventToDocument/Window in mere dom

--- packages-dom/mere-dom/src/helper/bind-event-to-document.ts
+++ packages-dom/mere-dom/src/helper/bind-event-to-document.ts
@@ -1,5 +1,5 @@
 /**
- * 全局绑定事件到 document
+ * 全局绑定事件到 document，返回解绑的无参方法
  */
 export default function bindEventToDocument<T extends keyof DocumentEventMap>(type: T, listener: (e: DocumentEventMap[T]) => void, options?: boolean | AddEventListenerOptions): () => void {
   document.addEventListener(type, listener, options);
--- packages-dom/mere-dom/src/helper/bind-event-to-window.ts
+++ packages-dom/mere-dom/src/helper/bind-event-to-window.ts
@@ -1,5 +1,5 @@
 /**
- * 全局绑定事件到 window
+ * 全局绑定事件到 window，返回解绑的无参方法
  */
 export default function bindEventToWindow<T extends keyof WindowEventMap>(type: T, listener: (e: WindowEventMap[T]) => void, options?: boolean | AddEventListenerOptions): () => void {
   window.addEventListener(type, listener, options);
