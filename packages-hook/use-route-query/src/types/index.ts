3dee2685541ec9fa5ea4097881642afd1bcd051e
jianchun.wang <justnewbee@gmail.com>
2026-03-06 11:19:46 +0800

    chore: up versions ✨

--- packages-hook/use-route-query/src/types/index.ts
+++ packages-hook/use-route-query/src/types/index.ts
@@ -1,1 +1,1 @@
-export type TUseRouteQueryResult<T extends object> = [Required<T>, (paramsUpdate: Partial<T>) => void];
+export type TUseRouteQueryResult<T extends object> = [Required<T>, (paramsUpdate: Partial<T>, pathname?: string) => void];
