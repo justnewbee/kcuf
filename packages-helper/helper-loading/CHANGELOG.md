27b180fdc74f121f1cc94b533703093e24a64697
jianchun.wang <justnewbee@gmail.com>
2026-03-17 11:47:39 +0800

    feat: takePromiseWithLoading will set error to null when success

---
+++ packages-helper/helper-loading/CHANGELOG.md
@@ -0,0 +1,9 @@
+# CHANGELOG
+
+## 0.4.0 2026/03/17 @驳是
+
+* FEAT `takePromiseWithLoading` shall now set error to `null` when success
+
+## 0.3.0 2025/12/30 @驳是
+
+* FEAT Add `isLoading`
--- packages-helper/helper-loading/src/helper/take-promise-with-loading.ts
+++ packages-helper/helper-loading/src/helper/take-promise-with-loading.ts
@@ -4,7 +4,7 @@ import {
 
 type TSetData<T> = (data: T) => void;
 type TSetLoading = (loading: ELoadingStatus) => void;
-type TSetError = (error: Error) => void;
+type TSetError = (error: Error | null) => void;
 
 export default function takePromiseWithLoading<T>(promise: Promise<T>, setData: TSetData<T>, setLoading: TSetLoading, setError: TSetError): void {
   setLoading(ELoadingStatus.LOADING);
@@ -12,6 +12,7 @@ export default function takePromiseWithLoading<T>(promise: Promise<T>, setData: 
   promise.then(data => {
     setLoading(ELoadingStatus.SUCCESS);
     setData(data);
+    setError(null);
   }).catch((err: unknown) => {
     setLoading(ELoadingStatus.ERROR);
     setError(err as Error);
