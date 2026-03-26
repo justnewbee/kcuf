6ca82b6eccc68877329e2f538796871b0eb802da
jianchun.wang <justnewbee@gmail.com>
2026-03-26 16:33:52 +0800

    fix: added ignore selector

--- packages-hook/use-click-away/CHANGELOG.md
+++ packages-hook/use-click-away/CHANGELOG.md
@@ -1,9 +1,14 @@
 # CHANGELOG
 
+## 0.0.3 2026/03/26 @驳是
+
+* FIX Cannot use `click` for it will hide right after open
+* FEAT Added 2nd arg `ingore`
+
 ## 0.0.2 2026/03/26 @驳是
 
-* FIX changed event from `mousedown` to `click`
+* FIX Changed event from `mousedown` to `click`
 
 ## 0.0.1 2026/03/26 @驳是
 
-* FEAT first version
+* FEAT First version
--- packages-hook/use-click-away/src/use-click-away.ts
+++ packages-hook/use-click-away/src/use-click-away.ts
@@ -5,29 +5,34 @@ import {
   useEffect
 } from 'react';
 
-type TCallback = (e: MouseEvent) => void;
-
-export default function useClickAway<E extends Element = HTMLDivElement>(callback: TCallback): RefObject<E | null> {
+/**
+ * Registers a listener to the document to help you get notified when click away.
+ *
+ * @param callback - The callback function called on click-away
+ * @param ignore - The optional selector which can be ignored when click away
+ **/
+export default function useClickAway<E extends Element = HTMLDivElement>(callback: (e: MouseEvent) => void, ignore = '[data-click-away-ignore]'): RefObject<E | null> {
   const refElement = useRef<E>(null);
   const refCallback = useRef(callback);
   
   useLayoutEffect(() => {
     refCallback.current = callback;
-  });
+  }, [callback]);
   
   useEffect(() => {
     function handler(e: MouseEvent): void {
       const element = refElement.current;
+      const target = e.target as Element;
       
-      if (element && !element.contains(e.target as Element)) {
+      if (element && !element.contains(target) && (ignore && !target.closest(ignore))) {
         refCallback.current(e);
       }
     }
     
-    document.addEventListener('click', handler);
+    document.addEventListener('mousedown', handler); // 不能用 click，否则可能导致出来就消失
     
-    return () => document.removeEventListener('click', handler);
-  }, []);
+    return () => document.removeEventListener('mousedown', handler);
+  }, [ignore]);
   
   return refElement;
 }
