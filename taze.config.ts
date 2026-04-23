d68abca966a3fc188b678d3fea8af6da76a136dc
jianchun.wang <justnewbee@gmail.com>
2026-04-23 17:45:04 +0800

    chore: up versions

--- taze.config.ts
+++ taze.config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
   // ignorePaths: [],
   exclude: [ // ignore packages from bumping
     'unfetch', // 5.0 输出的是 `.mjs` 对构建有要求...
-    'immer', // TODO up 11
     'eslint' // TODO up 10 - https://github.com/jsx-eslint/eslint-plugin-react/issues/3977
   ]
 });
