e2a52de19f456cbb29f63378d17ca99776ab78b2
jianchun.wang <justnewbee@gmail.com>
2026-05-11 16:21:48 +0800

    fix: normalize id will only return empty when 0

--- packages-helper/helper-data/src/helper/normalize-id.ts
+++ packages-helper/helper-data/src/helper/normalize-id.ts
@@ -2,11 +2,15 @@
  * 标准化 ID 值
  */
 export default function normalizeId(id0: unknown): string {
+  if (!id0) { // 0 '' null undefined NaN...
+    return '';
+  }
+  
   if (typeof id0 === 'string') {
     return id0;
   }
   
-  if (typeof id0 === 'number' && id0 !== 0) {
+  if (typeof id0 === 'number') {
     return id0.toString();
   }
   
