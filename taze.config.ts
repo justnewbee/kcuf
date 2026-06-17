0efa02c79e1842d1eeed4bc460d6d70a551c8c89
jianchun.wang <justnewbee@gmail.com>
2026-06-17 16:47:57 +0800

    chore: up versions ✨

--- taze.config.ts
+++ taze.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   // ignorePaths: [],
   exclude: [ // ignore packages from bumping
     'unfetch', // 5.0 输出的是 `.mjs` 对构建有要求...
-    'eslint' // TODO up 10 - https://github.com/jsx-eslint/eslint-plugin-react/issues/3977
+    'eslint', // TODO up 10 - https://github.com/jsx-eslint/eslint-plugin-react/issues/3977
+    '@babel/*' // 暂时不能升级到 8，因为依赖包（如 antd）用到的 @babel/runtime 会导致构建失败
   ]
 });
